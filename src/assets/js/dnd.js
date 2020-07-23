9dbe04205076f37bea4d3059ba38855793d132f5
Ambert Jean-Christophe <jc.ambert@free.fr>
2020-07-23 10:55:57 +0200

    Second Commit

--- src/assets/js/dnd.js
+++ src/assets/js/dnd.js
@@ -27,9 +27,12 @@ import { takeUntil, mergeMap, flatMap, map, merge, switchMap } from 'rxjs/operat
     }
 })(typeof window !== "undefined" ? window : this, function (window, noGlobal) {
     "use strict";
+    var self=this;
+    var document=window.document;
     var onDragStart = function (evt, ctx) {
         console.log("Drag Start:", evt);
-        evt.dataTransfer.setData("text", evt.target.id);
+        self.draggedElement=evt.target;
+        evt.dataTransfer.setData("text/plain", evt.target.id);
     }
     var onDragEnter = function (evt, ctx) {
         console.log("Drag Enter:", evt);
@@ -49,26 +52,53 @@ import { takeUntil, mergeMap, flatMap, map, merge, switchMap } from 'rxjs/operat
             evt.target.classList.remove(ctx.droppablehover);
         }
     }
-    var onDrop = function (evt, ctx) {
+    var onDrop = function (evt,allowDropCb, ctx) {
         console.log("drop:", evt);
+        event.preventDefault(); // This is in order to prevent the browser default handling of the data
+        event.target.classList.remove(ctx.droppablehover);
+        const draggableElementData = event.dataTransfer.getData("text"); // Get the dragged data. This method will return any data that was set to the same type in the setData() method
+        const droppableElementData = event.target.getAttribute("data-draggable-id");
+        if(allowDropCb(draggableElementData,droppableElementData))
+            ctx.onDropped(evt,ctx,draggableElementData,droppableElementData)
+
+    }
+    var onDropped=function(evt,ctx,from,to){
+        console.log(from,to);
+        const draggableElement = document.getElementById(from);
+        const droppableElement = document.getElementById(to);
+        evt.target.classList.add(ctx.dropped);
+        evt.target.style.backgroundColor = window.getComputedStyle(draggableElement).color;
+        draggableElement.classList.add(ctx.dragged);
+        draggableElement.setAttribute("draggable", "false");
+        evt.target.insertAdjacentHTML("afterbegin", `<i class="fas fa-${from}"></i>`);
+
+        droppableElement.setAttribute("draggable", "true");
+        console.log("On Dropped",droppableElement);
+    
+
     }
     var version = "1.0.0",
         options = {
             draggable: ".draggable",
             droppable: ".droppable",
+            dragged:"dragged",
             dropped: "dropped",
             droppablehover: "droppable-hover",
             onDragStart: onDragStart,
             onDragEnter: onDragEnter,
             onDragOver: onDragOver,
             onDragLeave: onDragLeave,
-            onDrop: onDrop
+            onDrop: onDrop,
+            onDropped:onDropped
         },
         DND = function (selector, opts) {
             //console.log("DND Constructed");
+            if(selector==null ||selector=="undefined")
+                selector=document;
             const o = Object.assign({}, options, opts)
-            const draggables = window.document.querySelectorAll(o.draggable);
-            const droppables = window.document.querySelectorAll(o.droppable);
+            const draggables = selector.querySelectorAll(o.draggable);
+            const droppables = selector.querySelectorAll(o.droppable);
+            draggables.forEach(d=>d.draggable=true);
             //console.log(draggables);
             const dragStart = fromEvent(draggables, 'dragstart');
             const dragEnter = fromEvent(droppables, 'dragenter');
@@ -80,7 +110,7 @@ import { takeUntil, mergeMap, flatMap, map, merge, switchMap } from 'rxjs/operat
             dragEnter.subscribe(evt => o.onDragEnter(evt, o));
             dragOver.subscribe(evt => o.onDragOver(evt, o));
             dragLeave.subscribe(evt => o.onDragLeave(evt,o));
-            drop.subscribe(evt => o.onDrop(evt, o));
+            drop.subscribe(evt => o.onDrop(evt,(from,to)=>from==to, o));
         };
     DND.fn = DND.prototype = {
         version: version,
--- src/components/DragDrop.vue
+++ src/components/DragDrop.vue
@@ -1,38 +1,38 @@
 <template>
   <div>
     <section class="draggable-elements">
-      <i class="fas fa-cat draggable" draggable="true" style="color: #ff6384;" id="cat"></i>
-      <i class="fas fa-dog draggable" draggable="true" style="color: #36a2eb;" id="dog"></i>
-      <i class="fas fa-dove draggable" draggable="true" style="color: #ffce56;" id="dove"></i>
-      <i class="fas fa-fish draggable" draggable="true" style="color: #9966ff;" id="fish"></i>
-      <i class="fas fa-frog draggable" draggable="true" style="color: #4bc0c0;" id="frog"></i>
-      <i class="fas fa-horse draggable" draggable="true" style="color: #333333;" id="horse"></i>
-      <i class="fas fa-hippo draggable" draggable="true" style="color: #ff9f40;" id="hippo"></i>
-      <i class="fas fa-spider draggable" draggable="true" style="color: #ff99cc;" id="spider"></i>
+      <i class="fas fa-cat draggable" style="color: #ff6384;" id="cat"></i>
+      <i class="fas fa-dog draggable"  style="color: #36a2eb;" id="dog"></i>
+      <i class="fas fa-dove draggable"  style="color: #ffce56;" id="dove"></i>
+      <i class="fas fa-fish draggable"  style="color: #9966ff;" id="fish"></i>
+      <i class="fas fa-frog draggable"  style="color: #4bc0c0;" id="frog"></i>
+      <i class="fas fa-horse draggable"  style="color: #333333;" id="horse"></i>
+      <i class="fas fa-hippo draggable"  style="color: #ff9f40;" id="hippo"></i>
+      <i class="fas fa-spider draggable" style="color: #ff99cc;" id="spider"></i>
     </section>
     <section class="droppable-elements">
-      <div class="droppable" data-draggable-id="frog">
+      <div class="droppable" data-draggable-id="frog" draggable="false">
         <span>Frog</span>
       </div>
-      <div class="droppable" data-draggable-id="dove">
+      <div class="droppable" data-draggable-id="dove" draggable="false">
         <span>Bird</span>
       </div>
-      <div class="droppable" data-draggable-id="cat">
+      <div class="droppable" data-draggable-id="cat" draggable="false">
         <span>Cat</span>
       </div>
-      <div class="droppable" data-draggable-id="fish">
+      <div class="droppable" data-draggable-id="fish" draggable="false">
         <span>Fish</span>
       </div>
-      <div class="droppable" data-draggable-id="dog">
+      <div class="droppable" data-draggable-id="dog" draggable="false">
         <span>Dog</span>
       </div>
-      <div class="droppable" data-draggable-id="spider">
+      <div class="droppable" data-draggable-id="spider" draggable="false">
         <span>Spider</span>
       </div>
-      <div class="droppable" data-draggable-id="horse">
+      <div class="droppable" data-draggable-id="horse" draggable="false">
         <span>Horse</span>
       </div>
-      <div class="droppable" data-draggable-id="hippo">
+      <div class="droppable" data-draggable-id="hippo" draggable="false">
         <span>Hippo</span>
       </div>
     </section>
